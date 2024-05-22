95772bf58c43911ba1bacd9fa566d4783ae5b7d0
Joe Gene Quesada <52223592+apcjlquesada@users.noreply.github.com>
2024-05-22 13:37:29 +0000

    Step 4

---
+++ laravel-inertia-vue-app/vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: 'resources/js/app.js',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    server: {
+        hmr: {
+            host: process.env.CODESPACE_NAME ? process.env.CODESPACE_NAME + '-5173.app.github.dev' : null,
+            clientPort: process.env.CODESPACE_NAME ? 443 : null,
+            protocol: process.env.CODESPACE_NAME ? 'wss' : null
+        },
+    }
+});
